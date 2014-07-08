cd0572637bb5c621672061f9b1c2f368f77a0e97
John Ritter <rittjoh@yahoo.com>
2014-07-08 12:28:02 -0400

    third commit

--- programs/server/app/both/collections/uploads.js
+++ programs/server/app/both/collections/uploads.js
@@ -6,7 +6,7 @@ Uploads = new FS.Collection("uploads", {
 */
 
 uploadStore = new FS.Store.FileSystem("uploads", {
-  path: "~/tasks/public", //optional, default is "/cfs/files" path within app container
+  //path: "~/tasks/public", //optional, default is "/cfs/files" path within app container
   //transformWrite: myTransformWriteFunction, //optional
   //transformRead: myTransformReadFunction, //optional
   //maxTries: 1 //optional, default 5
